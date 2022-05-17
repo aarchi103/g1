a89140f4b558bd9e727b567c50feeb7d17966bcc
ACHINTYABHATIA <achintya0072.be21@chitkara.edu.in>
2022-05-17 13:56:53 +0530

    changed first.cpp

--- first.cpp
+++ first.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+i#include <iostream>
 using namespace std;
 int main(){
 cout<<"hello world"<<endl;
 cout<<"hello world world"<<endl;
 cout<<"hello world1"<<endl;
-}
+cout<<"hello sir"<<endl;}
